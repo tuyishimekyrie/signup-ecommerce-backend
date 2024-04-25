ba38d24b0adcd6914886fdb5a289254a95caac5b
MUGABO Andre <andremugabo@yahoo.fr>
2024-04-25 16:36:12 +0200

    fix(package.json): Move parserOptions
    - Removed "parserOptions" from package.json
    - Moved "parserOptions" configuration to eslint.config.mjs
    
    [finishes #187364841]

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,45 +1,49 @@
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
 
-import path from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import pluginJs from "@eslint/js";
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import pluginJs from '@eslint/js'
 
 // mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: pluginJs.configs.recommended,
-});
+})
 
 export default [
   { languageOptions: { globals: globals.browser } },
-  ...compat.extends("standard-with-typescript"),
+  ...compat.extends('standard-with-typescript'),
   ...tseslint.configs.recommended,
   pluginReactConfig,
   {
     root: true,
-    extends: "airbnb-base",
+    extends: 'airbnb-base',
     env: {
       node: true,
       es6: true,
       jest: true,
     },
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
     rules: {
-      "one-var": 0,
-      "one-var-declaration-per-line": 0,
-      "new-cap": 0,
-      "consistent-return": 0,
-      "no-param-reassign": 0,
-      "comma-dangle": 0,
-      curly: ["error", "multi-line"],
-      "import/no-unresolved": [2, { commonjs: true }],
-      "no-shadow": ["error", { allow: ["req", "res", "err"] }],
-      "valid-jsdoc": [
-        "error",
+      'one-var': 0,
+      'one-var-declaration-per-line': 0,
+      'new-cap': 0,
+      'consistent-return': 0,
+      'no-param-reassign': 0,
+      'comma-dangle': 0,
+      curly: ['error', 'multi-line'],
+      'import/no-unresolved': [2, { commonjs: true }],
+      'no-shadow': ['error', { allow: ['req', 'res', 'err'] }],
+      'valid-jsdoc': [
+        'error',
         {
           requireReturn: true,
           requireReturnType: true,
@@ -47,8 +51,8 @@ export default [
           requireReturnDescription: true,
         },
       ],
-      "require-jsdoc": [
-        "error",
+      'require-jsdoc': [
+        'error',
         {
           require: {
             FunctionDeclaration: true,
@@ -59,4 +63,4 @@ export default [
       ],
     },
   },
-];
+]
